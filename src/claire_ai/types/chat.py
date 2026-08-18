9ff2f02167199b0c71c0615b770e6566a4235059
Andrew Gabriel Santos Souza <andrew.souza@horuscda.com.br>
2026-08-18 18:08:54 -0300

    feat: adicionando progressão

--- src/claire_ai/types/chat.py
+++ src/claire_ai/types/chat.py
@@ -1,6 +1,7 @@
 from typing import Annotated
 import typer
 from rich import print as rich_print
+from rich.progress import Progress, SpinnerColumn, TextColumn
 
 from claire_ai.services.chat_service import ChatService
 
@@ -8,7 +9,13 @@ chat_app = typer.Typer()
 
 @chat_app.command()
 def chat(query: Annotated[str, typer.Option(prompt="Ask me anything")]):
-    chat_service = ChatService()
+    with Progress(
+        SpinnerColumn(),
+        TextColumn("[progress.description]{task.description}"),
+        transient=True,
+    ) as progress:
+        progress.add_task(description="Thinking...", total=None)
+        chat_service = ChatService()
 
-    ai_response = chat_service.invoke_ai_response(query)
+        ai_response = chat_service.invoke_ai_response(query)
     rich_print(ai_response)
